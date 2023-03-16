ea99505901ef04a470ff6fc0c68ec07a446bc36d
mgd666 <1291788563@qq.com>
2023-03-16 15:33:54 +0800

    finished Commit and Stage

--- proj2/gitlet/Commit.java
+++ proj2/gitlet/Commit.java
@@ -2,8 +2,11 @@ package gitlet;
 
 // TODO: any imports you need here
 
-import java.util.Date; // TODO: You'll likely use this in this class
-import java.util.List;
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
+import java.util.*;
+
+import static gitlet.Utils.sha1;
 
 /** Represents a gitlet commit object.
  *  TODO: It's a good idea to give a description here of what else this Class
@@ -23,8 +26,80 @@ public class Commit {
     /** The message of this Commit. */
     private String message;
     private Date currentTime;
-    List<String> parentHashCodes;
-    List<String> blobCodes;
+    private List<String> parent;
+    private HashMap<String, String> blobsMap;
+    private String id;
+
+    public String getMessage() {
+        return message;
+    }
+
+    public String getCurrentTime() {
+        DateFormat df = new SimpleDateFormat("EEE MMM d HH:mm:ss yyyy Z", Locale.ENGLISH);
+        return df.format(currentTime);
+    }
+
+    public List<String> getParent() {
+        return parent;
+    }
+
+    public String getFirstParentId() {
+        if (parent.isEmpty()) {
+            return "null";
+        }
+        return parent.get(0);
+    }
+
+    public HashMap<String, String> getBlobsMap() {
+        return blobsMap;
+    }
+
+    public String getId() {
+        return id;
+    }
 
     /* TODO: fill in the rest of this class. */
+
+    public Commit() {
+        this.message = "initial message";
+        this.currentTime = new Date(0);
+        this.id = sha1(message,currentTime.toString());
+        this.parent = new LinkedList<>();
+        this.blobsMap = new HashMap<>();
+    }
+
+    public Commit(String message, List<Commit> parents,Stage stage) {
+        this.message = message;
+        this.currentTime = new Date();
+        this.parent = new ArrayList<>(2);
+        for (Commit p : parents) {
+            this.parent.add(p.getId());
+        }
+        this.blobsMap = parents.get(0).getBlobsMap();
+
+        for (Map.Entry<String, String> item : stage.getAddStage().entrySet()) {
+            String filename = item.getKey();
+            String blobId = item.getValue();
+            blobsMap.put(filename,blobId);
+        }
+
+        for (String fileName : stage.getRemovedStage()) {
+            blobsMap.remove(fileName);
+        }
+
+        this.id = sha1(message,currentTime.toString(),parents.toString(),blobsMap.toString());
+    }
+
+    public String getCommitAsString() {
+        StringBuffer sb = new StringBuffer();
+        sb.append("===\n");
+        sb.append("commit " + this.id + "\n");
+        if (parent.size() == 2) {
+            sb.append("Merge: " + parent.get(0).substring(0,7) + " " + parent.get(1).substring(0,7) + "\n");
+        }
+        sb.append("Date: " + this.getCurrentTime() + "\n");
+        sb.append(this.message + "\n\n");
+        return sb.toString();
+    }
+
 }
--- proj2/gitlet/Stage.java
+++ proj2/gitlet/Stage.java
@@ -1,7 +1,52 @@
 package gitlet;
 
+import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.HashSet;
 import java.util.List;
 
-public class Stage {
-    List<Blob> blobs;
+public class Stage implements Serializable {
+    /**
+     * <filename, blob's id>
+     */
+    private HashMap<String, String> addStage;
+    /**
+     * filename
+     */
+    private HashSet<String> removedStage;
+
+    public Stage() {
+        addStage = new HashMap<>();
+        removedStage = new HashSet<>();
+    }
+
+    public void addFile(String file, String blobId) {
+        addStage.put(file,blobId);
+        removedStage.add(file);
+    }
+
+    public void removeFile(String file) {
+        addStage.remove(file);
+        removedStage.add(file);
+    }
+
+    public HashMap<String, String> getAddStage() {
+        return addStage;
+    }
+
+    public HashSet<String> getRemovedStage() {
+        return removedStage;
+    }
+
+    public ArrayList<String> getStageFileNames() {
+        ArrayList<String> res = new ArrayList<>();
+        res.addAll(addStage.keySet());
+        res.addAll(removedStage);
+        return res;
+    }
+
+    public boolean isEmpty() {
+        return addStage.isEmpty() && removedStage.isEmpty();
+    }
 }
